8a4e8fa0f43b18c4baaf4a52012b5c49be40b1ec
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-11-19 12:35:42 +0000

    Complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,14 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @members = ["elorm", "enam", "eyram", "kplorla"]
+    search = params[:member]
+
+    if search
+      @members = @members.select do |member|
+        member.start_with? search.downcase
+      end
+    end
   end
 
   def home
